d3ceaab6f6d68899e5cd92e1f544a35a24c40579
Lauren White <lauren5826@gmail.com>
2018-07-10 15:45:39 -0700

    Implement DontRepeatTypeInStaticProperties rule and tests. (#78)

--- Sources/Rules/DontRepeatTypeInStaticProperties.swift
+++ Sources/Rules/DontRepeatTypeInStaticProperties.swift
@@ -12,5 +12,77 @@ import SwiftSyntax
 ///
 /// - SeeAlso: https://google.github.io/swift#static-and-class-properties
 public final class DontRepeatTypeInStaticProperties: SyntaxLintRule {
+  
+  public override func visit(_ node: ClassDeclSyntax) {
+    determinePropertyNameViolations(members: node.members.members, nodeId: node.identifier.text)
+  }
+  
+  public override func visit(_ node: EnumDeclSyntax) {
+    determinePropertyNameViolations(members: node.members.members, nodeId: node.identifier.text)
+  }
+  
+  public override func visit(_ node: ProtocolDeclSyntax) {
+    determinePropertyNameViolations(members: node.members.members, nodeId: node.identifier.text)
+  }
+  
+  public override func visit(_ node: StructDeclSyntax) {
+    determinePropertyNameViolations(members: node.members.members, nodeId: node.identifier.text)
+  }
+  
+  public override func visit(_ node: ExtensionDeclSyntax) {
+    determinePropertyNameViolations(members: node.members.members, nodeId: node.extendedType.description)
+  }
+  
+  func determinePropertyNameViolations(members: MemberDeclListSyntax, nodeId: String) {
+    for member in members {
+      guard let decl = member.decl as? VariableDeclSyntax else { continue }
+      guard let modifiers = decl.modifiers else { continue }
+      guard containsClassOrStatic(modifiers: modifiers) else { continue }
+      
+      let typeName = withoutPrefix(name: nodeId)
+      var varName = ""
+      
+      for binding in decl.bindings {
+        guard let exp = binding.pattern as? IdentifierPatternSyntax else { continue }
+        varName = exp.identifier.text
+      }
+      
+      if varName.contains(typeName) {
+        diagnose(.removeTypeFromName(name: varName, type: typeName), on: decl)
+      }
+    }
+  }
+  
+  func containsClassOrStatic(modifiers: ModifierListSyntax) -> Bool {
+    for modifier in modifiers {
+      let name = modifier.name.text
+      if name == "class" || name == "static" { return true }
+    }
+    return false
+  }
+  
+  // Returns the given string without capitalized prefix in the beginning
+  func withoutPrefix(name: String) -> String {
+    let formattedName = name.trimmingCharacters(in: CharacterSet.whitespaces)
+    let upperCase = Array(formattedName.uppercased())
+    let original = Array(formattedName)
+    guard original[0] == upperCase[0] else { return name }
+    
+    var prefixEndsAt = 0
+    var idx = 0
+    while idx <= name.count - 2 {
+      if original[idx] == upperCase[idx] &&
+         original[idx + 1] != upperCase[idx + 1] {
+        prefixEndsAt = idx
+      }
+      idx += 1
+    }
+    return String(formattedName.dropFirst(prefixEndsAt))
+  }
+}
 
+extension Diagnostic.Message {
+  static func removeTypeFromName(name: String, type: String) -> Diagnostic.Message {
+    return .init(.warning, "Remove '\(type)' from '\(name)'")
+  }
 }
