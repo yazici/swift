a67d563f68302689826cd954a949abfd6c0caa2e
Lauren White <lauren5826@gmail.com>
2018-07-19 08:45:21 -0700

    Implement group numeric literals (#79)

--- Sources/Rules/GroupNumericLiterals.swift
+++ Sources/Rules/GroupNumericLiterals.swift
@@ -10,7 +10,67 @@ import SwiftSyntax
 ///
 /// Format: All numeric literals that should be grouped will have `_`s inserted where appropriate.
 ///
+/// TODO: Minimum numeric literal length bounds and numeric groupings selected arbitrarily, could
+///       be  reevaluated.
+///
+/// TODO: Handle floating point literals
+///
 /// - SeeAlso: https://google.github.io/swift#numeric-literals
 public final class GroupNumericLiterals: SyntaxFormatRule {
+  public override func visit(_ node: IntegerLiteralExprSyntax) -> ExprSyntax {
+    
+    var digits = node.digits.text
+    guard !digits.contains("_") else { return node }
+    
+    let isNegative = digits.first == "-"
+    digits = isNegative ? String(digits.dropFirst()) : digits
+    
+    var newDigits = ""
+    
+    switch digits.prefix(2) {
+    case "0x":
+      // Hexadecimal
+      let digitsNoPrefix = String(digits.dropFirst(2))
+      guard let intDigits = Int(digitsNoPrefix, radix: 16) else { return node }
+      guard intDigits >= 0x1000_0000 else { return node }
+      diagnose(.groupNumericLiteral(byStride: 4), on: node)
+      newDigits = "0x" + groupDigitsByStride(digits: digitsNoPrefix, stride: 4)
+    case "0b":
+      // Binary
+      let digitsNoPrefix = String(digits.dropFirst(2))
+      guard let intDigits = Int(digitsNoPrefix, radix: 2) else { return node }
+      guard intDigits >= 0b1_000000000 else { return node }
+      diagnose(.groupNumericLiteral(byStride: 8), on: node)
+      newDigits = "0b" + groupDigitsByStride(digits: digitsNoPrefix, stride: 8)
+    case "0o":
+      // Octal
+      return node
+    default:
+      // Decimal
+      guard let intDigits = Int(digits) else { return node }
+      guard intDigits >= 1_000_000 else { return node }
+      diagnose(.groupNumericLiteral(byStride: 3), on: node)
+      newDigits = groupDigitsByStride(digits: digits, stride: 3)
+    }
+    
+    newDigits = isNegative ? "-" + newDigits : newDigits
+    return node.withDigits(SyntaxFactory.makeIdentifier(newDigits))
+  }
+  
+  func groupDigitsByStride(digits: String, stride: Int) -> String {
+    var newGrouping = Array(digits)
+    var i = 1
+    while i * stride < digits.count {
+      newGrouping.insert("_", at: digits.count - i * stride)
+      i += 1
+    }
+    return String(newGrouping)
+  }
+}
 
+extension Diagnostic.Message {
+  static func groupNumericLiteral(byStride: Int) -> Diagnostic.Message {
+    let ending = byStride == 3 ? "rd" : "th"
+    return .init(.warning, "group numeric literal using '_' every \(byStride)\(ending) number")
+  }
 }
