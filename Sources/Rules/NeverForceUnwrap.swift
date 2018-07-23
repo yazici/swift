35e178922d91c32bfb541c72b0362bb101418086
Lauren White <lauren5826@gmail.com>
2018-07-23 10:01:38 -0700

    Implement never force unwrap (#77)

--- Sources/Rules/NeverForceUnwrap.swift
+++ Sources/Rules/NeverForceUnwrap.swift
@@ -8,5 +8,29 @@ import SwiftSyntax
 ///
 /// - SeeAlso: https://google.github.io/swift#force-unwrapping-and-force-casts
 public final class NeverForceUnwrap: SyntaxLintRule {
+  
+  // Checks if "XCTest" is an import statement
+  public override func visit(_ node: SourceFileSyntax) {
+    setImportsXCTest(context: context, sourceFile: node)
+    super.visit(node)
+  }
+  
+  public override func visit(_ node: ForcedValueExprSyntax) {
+    guard !context.importsXCTest else { return }
+    diagnose(.doNotForceUnwrap(name: node.expression.description), on: node)
+  }
+  
+  public override func visit(_ node: AsExprSyntax) {
+    guard !context.importsXCTest else { return }
+    diagnose(.doNotForceCast(name: node.typeName.description), on: node)
+  }
+}
 
+extension Diagnostic.Message {
+  static func doNotForceUnwrap(name: String) -> Diagnostic.Message {
+    return .init(.warning, "do not force unwrap '\(name)'")
+  }
+  static func doNotForceCast(name: String) -> Diagnostic.Message {
+    return .init(.warning, "do not force cast to '\(name)'")
+  }
 }
