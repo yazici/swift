4f353cebac5092cadc0d6220852a0be5adf46d98
Austin Belknap <dabelknap@google.com>
2018-10-11 09:45:25 -0700

    Add support for tuple declarations.

---
+++ Tests/PrettyPrinterTests/TupleDeclTests.swift
@@ -0,0 +1,37 @@
+public class TupleDeclTests: PrettyPrintTestCase {
+  public func testBasicTuples() {
+    let input =
+      """
+      let a = (1, 2, 3)
+      let a: (Int, Int, Int) = (1, 2, 3)
+      let a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+      let a = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+      """
+
+    let expected =
+      """
+      let a = (1, 2, 3)
+      let a: (Int, Int, Int) = (1, 2, 3)
+      let a = (
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+      )
+      let a = (
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12
+      )
+
+      """
+
+    assertPrettyPrintEqual(input: input, expected: expected, linelength: 37)
+  }
+}
