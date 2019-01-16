92898da81c0933cf9b4aaffde1955d146e793cb5
Austin Belknap <dabelknap@google.com>
2019-01-15 16:26:11 -0800

    Don't remove 'get' statements with modifiers or attributes.

--- Tests/SwiftFormatRulesTests/UseSingleLinePropertyGetterTests.swift
+++ Tests/SwiftFormatRulesTests/UseSingleLinePropertyGetterTests.swift
@@ -19,6 +19,9 @@ public class UseSingleLinePropertyGetterTests: DiagnosingTestCase {
                get { return 0 }
                set { print("no set, only get") }
              }
+             var j: Int {
+               mutating get { return 0 }
+             }
              """,
       expected: """
                 var g: Int { return 4 }
@@ -28,6 +31,9 @@ public class UseSingleLinePropertyGetterTests: DiagnosingTestCase {
                 var i: Int {
                   get { return 0 }
                   set { print("no set, only get") }
+                }
+                var j: Int {
+                  mutating get { return 0 }
                 }
                 """)
   }
