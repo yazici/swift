4345f8d1ac49cff93f2177aec44f63005b2556e0
Tony Allevato <allevato@google.com>
2019-01-23 12:01:23 -0800

    Improve the formatter configuration.
    - Add a --configuration option that lets the user pass the path to a
      JSON file that will be loaded instead of the default configuration.
    - Add a new mode (--mode=dump-configuration) that dumps the default
      configuration as JSON to standard output, so users can start with
      that and edit it.
    - Make `Indent` an enum instead of a struct.
    - Manually write the decode/encode methods (sigh) so that values can
      be omitted from the JSON (by default, decoding throws an error if
      a value is missing, even if the default value is present in the
      property initializer).

--- Sources/swift-format/main.swift
+++ Sources/swift-format/main.swift
@@ -20,12 +20,14 @@ import Utility
 enum Mode: String, Codable, ArgumentKind {
   case format
   case lint
+  case dumpConfiguration = "dump-configuration"
   case version
 
   static var completion: ShellCompletion {
     return .values([
       ("format", "Format the provided files."),
       ("lint", "Lint the provided files."),
+      ("dump-configuration", "Dump the default configuration as JSON to standard output."),
     ])
   }
 
@@ -38,7 +40,7 @@ enum Mode: String, Codable, ArgumentKind {
 }
 
 struct CommandLineOptions: Codable {
-  var configurationPath = ""
+  var configurationPath: String? = nil
   var paths: [String] = []
   var verboseLevel = 0
   var mode: Mode = .format
@@ -56,7 +58,7 @@ func processArguments(commandName: String, _ arguments: [String]) -> CommandLine
       option: "--mode",
       shortName: "-m",
       kind: Mode.self,
-      usage: "The mode to run swift-format in. Either 'format' or 'lint'."
+      usage: "The mode to run swift-format in. Either 'format', 'lint', or 'dump-configuration'."
   )) {
     $0.mode = $1
   }
@@ -73,6 +75,7 @@ func processArguments(commandName: String, _ arguments: [String]) -> CommandLine
     positional: parser.add(
       positional: "filenames or paths",
       kind: [String].self,
+      optional: true,
       strategy: .upToNextOption,
       usage: "One or more input filenames",
       completion: .filename
@@ -96,6 +99,14 @@ func processArguments(commandName: String, _ arguments: [String]) -> CommandLine
   )) {
     $0.printTokenStream = $1
   }
+  binder.bind(
+    option: parser.add(
+      option: "--configuration",
+      kind: String.self,
+      usage: "The path to a JSON file containing the configuration of the linter/formatter."
+  )) {
+    $0.configurationPath = $1
+  }
 
   var opts = CommandLineOptions()
   do {
@@ -115,8 +126,10 @@ func main(_ arguments: [String]) -> Int32 {
   switch options.mode {
   case .format:
     var ret = 0
+    let configuration = decodedConfiguration(fromFileAtPath: options.configurationPath)
     for path in options.paths {
       ret |= formatMain(
+        configuration: configuration,
         path: path,
         prettyPrint: options.prettyPrint,
         printTokenStream: options.printTokenStream
@@ -125,13 +138,65 @@ func main(_ arguments: [String]) -> Int32 {
     return Int32(ret)
   case .lint:
     var ret = 0
+    let configuration = decodedConfiguration(fromFileAtPath: options.configurationPath)
     for path in options.paths {
-      ret |= lintMain(path: path)
+      ret |= lintMain(configuration: configuration, path: path)
     }
     return Int32(ret)
+  case .dumpConfiguration:
+    dumpDefaultConfiguration()
+    return 0
   case .version:
     print("0.0.1") // TODO(abl): Something fancy based on the git hash
     return 0
+  }
+}
+
+/// Loads and returns a `Configuration` from the given JSON file if it is found and is valid. If the
+/// file does not exist or there was an error decoding it, the program exits with a non-zero exit
+/// code.
+private func decodedConfiguration(fromFileAtPath path: String?) -> Configuration {
+  if let path = path {
+    do {
+      let url = URL(fileURLWithPath: path)
+      let data = try Data(contentsOf: url)
+      return try JSONDecoder().decode(Configuration.self, from: data)
+    }
+    catch {
+      // TODO: Improve error message, write to stderr.
+      print("Could not load configuration at \(path): \(error)")
+      exit(1)
+    }
+  }
+  else {
+    return Configuration()
+  }
+}
+
+/// Dumps the default configuration as JSON to standard output.
+private func dumpDefaultConfiguration() {
+  let configuration = Configuration()
+  do {
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = [.prettyPrinted]
+    if #available(macOS 10.13, *) {
+      encoder.outputFormatting.insert(.sortedKeys)
+    }
+
+    let data = try encoder.encode(configuration)
+    guard let jsonString = String(data: data, encoding: .utf8) else {
+      // This should never happen, but let's make sure we fail more gracefully than crashing, just
+      // in case.
+      // TODO: Improve error message, write to stderr.
+      print("Could not dump the default configuration: the JSON was not valid UTF-8")
+      exit(1)
+    }
+    print(jsonString)
+  }
+  catch {
+    // TODO: Improve error message, write to stderr.
+    print("Could not dump the default configuration: \(error)")
+    exit(1)
   }
 }
 
